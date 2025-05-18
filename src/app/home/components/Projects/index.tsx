b74e608e27a531421457e8be4cc1d2e5cb3e145e
Mutlu Kasap <mutlukasapweb@gmail.com>
2025-05-18 23:53:08 +0300

    Filtered repos.

--- src/app/home/components/Projects/index.tsx
+++ src/app/home/components/Projects/index.tsx
@@ -8,16 +8,16 @@ type Props = {
 };
 
 const Projects = (props: Props) => (
-  <section>
-    <Wrapper>
-      <SectionTitle title="Projects" href={'/projects'} />
-      <div className="grid gap-5 sm:grid-cols-2 md:grid-cols-3">
-        {props.repos.map((repository) => (
-          <Project repo={repository} key={repository.name} />
-        ))}
-      </div>
-    </Wrapper>
-  </section>
+    <section>
+        <Wrapper>
+            <SectionTitle title="Projects" href={'/projects'} />
+            <div className="grid gap-5 sm:grid-cols-2 md:grid-cols-3">
+                {props.repos.map((repository) => repository.name !== 'mutluksap' && !repository.fork && (
+                    <Project repo={repository} key={repository.name} />
+                ))}
+            </div>
+        </Wrapper>
+    </section>
 )
 
 export default Projects
