a54997a1ee046aa6f8a13c87b9eba9037398a189
Mutlu Kasap <mutlukasapweb@gmail.com>
2025-05-18 23:12:30 +0300

    Mail address changed.

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -2,5 +2,5 @@ export const socials = {
     twitter: "https://x.com/mutluksap",
     linkedin: "https://www.linkedin.com/in/mutlukasap/",
     github: "https://github.com/mutluksap",
-    mail: "mailto:mutlukasap@gmail.com",
+    mail: "mailto:mutlukasapweb@gmail.com",
 };
