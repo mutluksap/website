6296849bdaa7ab850f8a34f513b310be2fb834e6
Mutlu Kasap <mutlukasapweb@gmail.com>
2025-08-25 20:16:42 +0300

    Types refactor.

--- src/types/unsplash.ts
+++ src/types/unsplash.ts
@@ -1,17 +1,77 @@
 export interface PhotosResponse {
     id: string;
+    slug: string;
+    alternative_slugs: {
+        en: string;
+        es: string;
+        ja: string;
+        fr: string;
+        it: string;
+        ko: string;
+        de: string;
+        pt: string;
+        id: string;
+    };
+    created_at: string;
+    updated_at: string;
+    promoted_at: string | null;
+    width: number;
+    height: number;
+    color: string;
+    blur_hash: string;
+    description: string | null;
+    alt_description: string | null;
+    breadcrumbs: any[];
+    urls: {
+        raw: string;
+        full: string;
+        regular: string;
+        small: string;
+        thumb: string;
+        small_s3: string;
+    };
+    links: {
+        self: string;
+        html: string;
+        download: string;
+        download_location: string;
+    };
+    likes: number;
+    liked_by_user: boolean;
+    current_user_collections: any[];
+    sponsorship: null | object;
+    topic_submissions: Record<string, any>;
+    asset_type: string;
+    user: User;
+    pinned: null | string;
+}
+
+export interface User {
+    id: string;
     updated_at: string;
     username: string;
     name: string;
     first_name: string;
     last_name: string;
-    twitter_username: string;
-    portfolio_url: string;
+    twitter_username: string | null;
+    portfolio_url: string | null;
     bio: string | null;
-    location: string;
-    links: UserLinks;
-    profile_image: ProfileImage;
-    instagram_username: string;
+    location: string | null;
+    links: {
+        self: string;
+        html: string;
+        photos: string;
+        likes: string;
+        portfolio: string;
+        following: string;
+        followers: string;
+    };
+    profile_image: {
+        small: string;
+        medium: string;
+        large: string;
+    };
+    instagram_username: string | null;
     total_collections: number;
     total_likes: number;
     total_photos: number;
@@ -20,64 +80,10 @@ export interface PhotosResponse {
     total_promoted_illustrations: number;
     accepted_tos: boolean;
     for_hire: boolean;
-    social: SocialLinks;
-    photos: Photo[];
-    badge: unknown; // Eğer bu alan hep null ise: null
-    tags: Tags;
-    allow_messages: boolean;
-    numeric_id: number;
-    downloads: number;
-    meta: {
-        index: boolean;
+    social: {
+        instagram_username: string | null;
+        portfolio_url: string | null;
+        twitter_username: string | null;
+        paypal_email: string | null;
     };
-}
-
-export interface UserLinks {
-    self: string;
-    html: string;
-    photos: string;
-    likes: string;
-    portfolio: string;
-}
-
-export interface ProfileImage {
-    small: string;
-    medium: string;
-    large: string;
-}
-
-export interface SocialLinks {
-    instagram_username: string;
-    portfolio_url: string;
-    twitter_username: string;
-    paypal_email: string | null;
-}
-
-export interface Photo {
-    id: string;
-    slug: string;
-    created_at: string;
-    updated_at: string;
-    blur_hash: string;
-    asset_type: string;
-    urls: PhotoUrls;
-}
-
-export interface PhotoUrls {
-    raw: string;
-    full: string;
-    regular: string;
-    small: string;
-    thumb: string;
-    small_s3: string;
-}
-
-export interface Tags {
-    custom: Tag[];
-    aggregated: Tag[];
-}
-
-export interface Tag {
-    type: string;
-    title: string;
 }
